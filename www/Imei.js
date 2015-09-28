08d0cd1c37fb2e8ed645e0868933a5232bce2fbb
Abraham Cedeno <abraham.cedeno@vivacetek.com>
2015-09-28 14:59:57 -0430

    Success callback function parameter added

--- www/Imei.js
+++ www/Imei.js
@@ -2,7 +2,7 @@ var exec = require('cordova/exec');
 
 var Imei =
 {
-    getImei : function() { return exec(success, null, "Imei", "imei", []); }
+    getImei : function(success) { return exec(success, null, "Imei", "imei", []); }
 }
 
 module.exports = Imei;
