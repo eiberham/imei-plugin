d622d1f7efe12218698aa43e1012e459cc1142ac
Abraham Cedeno <abraham.cedeno@vivacetek.com>
2015-09-28 16:39:20 -0430

    Attempt to get the imei number on initialization for further usage

--- www/Imei.js
+++ www/Imei.js
@@ -1,8 +1,23 @@
 var exec = require('cordova/exec');
+var channel = require('cordova/channel');
 
-var Imei =
+/*var Imei =
 {
-    getImei : function(success) { return exec(success, null, "Imei", "imei", []); }
+    getImei : function(success) {
+        return exec(success, null, "Imei", "imei", []);
+    }
+}*/
+
+channel.waitForInitialization('onCordovaInfoReady');
+
+function Imei(){
+    this.imei = null;
+
+    channel.onCordovaReady.suscribe(function(){
+        exec(function(imei){
+            this.imei = imei;}, null, "Imei", "imei", []);
+    });
 }
 
-module.exports = Imei;
+module.exports = new Imei();
+//module.exports = Imei;
